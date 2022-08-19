9ac7ef06eb77b6e983d193c900dd461f5f4fd2f9
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-08-19 18:17:08 +0200

    fix full render route

---
+++ packages/backend/src/controllers/article-controller.js
@@ -0,0 +1,11 @@
+// import articles from '../db/db-init.js';
+//
+// const articleController = async (req, res) => {
+//   const { aid } = req.params;
+//   const data = await articles.findUnique({
+//     where: { articleId: aid },
+//   });
+//   res.json(data);
+// };
+//
+// export default articleController;
--- packages/backend/src/controllers/render-data-controller.js
+++ packages/backend/src/controllers/render-data-controller.js
@@ -1,9 +1,10 @@
 import { fastRender, fullRender } from '../services/select-for-render.js';
 
 const renderDataController = async (req, res) => {
+  const renderType = req.params.type?.toLowerCase();
   const fullAliases = ['full', 'expand', 'details'];
 
-  const showFull = fullAliases.includes(req.params.full.toLowerCase());
+  const showFull = fullAliases.includes(renderType);
   if (showFull) return res.json(await fullRender());
 
   res.json(await fastRender());
