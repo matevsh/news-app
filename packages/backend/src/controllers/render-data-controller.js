9097b25a2c090f11dfedfd88312b4c3fd967d052
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-08-19 16:21:38 +0200

    render route fix (lower case)

--- packages/backend/src/controllers/render-data-controller.js
+++ packages/backend/src/controllers/render-data-controller.js
@@ -3,7 +3,7 @@ import { fastRender, fullRender } from '../services/select-for-render.js';
 const renderDataController = async (req, res) => {
   const fullAliases = ['full', 'expand', 'details'];
 
-  const showFull = fullAliases.includes(req.params.full);
+  const showFull = fullAliases.includes(req.params.full.toLowerCase());
   if (showFull) return res.json(await fullRender());
 
   res.json(await fastRender());
