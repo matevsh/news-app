0b1daf5222d0a6ac97a726cbe8dfcd278ba66f50
Mateusz Marcinkowski <mateuszmarcinkowski44@gmail.com>
2022-08-19 18:30:48 +0200

    handling for single article

--- packages/backend/src/routes/api.js
+++ packages/backend/src/routes/api.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import renderDataController from '../controllers/render-data-controller.js';
 import updateArticlesController from '../controllers/update-articles-controller.js';
-// import articleController from '../controllers/article-controller.js';
+import articleController from '../controllers/article-controller.js';
 
 const router = Router();
 
 router.get('/render/:type?', renderDataController);
 router.get('/update', updateArticlesController);
 
-// router.get('/article/:aid', articleController);
+router.get('/article/:aid', articleController);
 
 router.get('*', (req, res) => res.json({ error: 404 }));
 
